0db3bc62123e11722602907206328d107d920808
Ben <ben@codeurs.be>
2024-11-22 09:45:42 +0100

    Update install.js
    If we install a package that is nested inside a single directory move the binaries up one level

--- install.js
+++ install.js
@@ -19,13 +19,18 @@ module.exports = function install(config, unpackedBinPath, os, arch) {
   }
   return binstall(url, unpackedBinPath).then(function() {
     var sub = fs.readdirSync(unpackedBinPath);
+    var moveFrom;
     if (sub.length === 1) {
-      var stats = fs.statSync(path.join(unpackedBinPath, sub[0]));
+      var inner = path.join(unpackedBinPath, sub[0])
+      var stats = fs.statSync(inner);
       if (stats.isDirectory()) {
-        unpackedBinPath = path.join(unpackedBinPath, sub[0]);
+        moveFrom = inner;
       }
     }
     config.binaries.forEach(function(bin) {
+      if (moveFrom)
+        fs.renameSync(path.join(moveFrom, bin + binExt), 
+                      path.join(unpackedBinPath, bin + binExt));
       fs.chmodSync(path.join(unpackedBinPath, bin + binExt), "755");
     });
   });
